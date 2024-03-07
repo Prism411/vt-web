bc0cf605ef42df2a778e59820c1e828fd658a865
Prism411 <jaderlouis@proton.me>
2024-03-07 16:54:09 -0300

    web working, without alarms

--- lib/screens/analysis.dart
+++ lib/screens/analysis.dart
@@ -1,3 +1,4 @@
+import 'dart:async'; // Importe a biblioteca dart:async
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:vtweb/services/call/model.dart'; // Substitua pelo caminho correto
@@ -14,11 +15,27 @@ class AnalysisScreen extends StatefulWidget {
 class _AnalysisScreenState extends State<AnalysisScreen> {
   List<UserData> usersData = [];
   int currentBlock = 1;
+  Timer? _timer; // Adiciona uma variável para o Timer
 
   @override
   void initState() {
     super.initState();
-    _fetchBlockData(currentBlock);
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel(); // Certifique-se de cancelar o timer quando o widget for desmontado
+    super.dispose();
+  }
+
+  void _startAutoFetch() {
+    // Cancela qualquer timer existente
+    _timer?.cancel();
+
+    // Cria um novo timer que chama _fetchBlockData a cada 10 Ssegundos
+    _timer = Timer.periodic(Duration(seconds: 2), (timer) {
+      _fetchBlockData(currentBlock++);
+    });
   }
 
   Future<void> _fetchBlockData(int bloco) async {
@@ -36,18 +53,12 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Análise de Usuários'),
+          title: Text('Análise de Usuários - Dia: $currentBlock'),
         backgroundColor: Colors.blue,
       ),
-      body: Column(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          if (usersData.isNotEmpty) // Verifica se a lista não está vazia
-            Image.network(
-              usersData.first.filepath,
-              width: 100,
-              height: 100,
-              alignment: Alignment.centerLeft,
-            ),
           Expanded(
             child: ListView.builder(
               itemCount: usersData.length,
@@ -59,17 +70,21 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
               },
             ),
           ),
+          if (usersData.isNotEmpty)
+            Align(
+              alignment: Alignment.centerRight,
+             child: Image.network(
+                'http://localhost:8080' + usersData.first.filepath, // Concatena a base URL com o filepath
+                width: 500,
+                height: 500,
+              ),
+            ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            currentBlock++;
-          });
-          _fetchBlockData(currentBlock);
-        },
-        tooltip: 'Carregar mais',
-        child: Icon(Icons.add),
+        onPressed: _startAutoFetch, // Atualiza para chamar _startAutoFetch
+        tooltip: 'Começar', 
+        child: Icon(Icons.play_arrow),
       ),
     );
   }
